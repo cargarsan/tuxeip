c3548f4f29d06e96d759204503875cbb8c836d09
stephane Jeanne <stephane.jeanne@gmail.com>
2015-03-09 14:14:33 +0100

    essai push google code

--- tuxeip/src/AB.h
+++ tuxeip/src/AB.h
@@ -1,6 +1,6 @@
 /***************************************************************************
- *   Copyright (C) 2006 by TuxPLC					                                 *
- *   Author Stephane JEANNE s.jeanne@tuxplc.net                            *
+ *   Copyright (C) 2006 by TuxPLC                                          *
+ *   Author Stephane JEANNE stephane.jeanne@gmail.com                      *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU General Public License as published by  *
